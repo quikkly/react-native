4784f996419d050763e0032e3e829b4810568f71
Janek Priimann <janek.priimann@gmail.com>
2019-07-03 16:28:49 +0300

    Allow to use scan view directly on Android

---
+++ android/src/main/java/net/quikkly/android/react/QuikklyScanFragment.java
@@ -0,0 +1,58 @@
+package net.quikkly.android.react;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.Log;
+import android.view.ViewParent;
+
+import com.facebook.react.bridge.Arguments;
+import com.facebook.react.bridge.ReactContext;
+import com.facebook.react.bridge.WritableMap;
+import com.facebook.react.uimanager.events.RCTEventEmitter;
+
+import net.quikkly.android.ScanResultListener;
+import net.quikkly.android.ui.ScanFragment;
+import net.quikkly.core.ScanResult;
+
+public class QuikklyScanFragment extends ScanFragment implements ScanResultListener {
+
+    private static final String TAG = QuikklyScanFragment.class.getSimpleName();
+
+    public QuikklyScanFragment() {
+        setScanResultListener(this);
+    }
+
+    @Override
+    public void onScanResult(@Nullable ScanResult scanResult) {
+        if(scanResult != null && scanResult.tags != null && scanResult.tags.length > 0) {
+            QuikklyScanView view = getScanView();
+            Context context = (view != null) ? view.getContext() : null;
+
+            if (context instanceof ReactContext) {
+                ReactContext reactContext = (ReactContext) context;
+                WritableMap event = Arguments.createMap();
+
+                event.putDouble("value", scanResult.tags[0].dataLong);
+
+                reactContext.getJSModule(RCTEventEmitter.class).receiveEvent(view.getId(), "scanCode", event);
+                Log.d(TAG, "Found code " + scanResult.tags[0].dataLong);
+            } else {
+                Log.e(TAG, "Unable to find react context");
+            }
+        }
+    }
+
+    private QuikklyScanView getScanView() {
+        ViewParent view = (ViewParent)getView();
+
+        while(view != null) {
+            if(view instanceof QuikklyScanView) {
+                return (QuikklyScanView)view;
+            }
+
+            view = view.getParent();
+        }
+
+        return null;
+    }
+}
--- android/src/main/java/net/quikkly/android/react/QuikklyScanViewManager.java
+++ android/src/main/java/net/quikkly/android/react/QuikklyScanViewManager.java
@@ -1,9 +1,13 @@
 package net.quikkly.android.react;
 
+import com.facebook.react.common.MapBuilder;
 import com.facebook.react.uimanager.SimpleViewManager;
 import com.facebook.react.uimanager.ThemedReactContext;
 
+import java.util.Map;
+
 import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 
 public class QuikklyScanViewManager extends SimpleViewManager<QuikklyScanView> {
 
@@ -13,6 +17,16 @@ public class QuikklyScanViewManager extends SimpleViewManager<QuikklyScanView> {
     @Override
     public String getName() {
         return REACT_CLASS;
+    }
+
+    @Nullable
+    @Override
+    @SuppressWarnings("unchecked")
+    public Map<String, Object> getExportedCustomBubblingEventTypeConstants() {
+        return (Map)MapBuilder.builder().put("scanCode",
+                MapBuilder.of("phasedRegistrationNames",
+                        MapBuilder.of("bubbled", "onScanCode")))
+                .build();
     }
 
     @Nonnull
